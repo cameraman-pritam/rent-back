2f8a1f72c5ac06c7e4173574f8dd4b1fae347d4e
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2026-03-28 21:15:42 +0530

    Feat: Added Icon, fixed spacing and alignment

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Card } from "primereact/card";
+import icon from "../assets/icon.png";
 
 const Root = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <>
+      <div className="flex items-stretch gap-4 justify-between">
+        <Card
+          title="Welcome"
+          subTitle="Elevate your living experience in just a click!"
+          className="md:w-25rem w-7/10"
+        >
+          <p>This is ...</p>
+        </Card>
+        <div className="w-3/10 h-3/10">
+          <img src={icon} className="rounded-3xl" preview />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { PrimeReactProvider } from "primereact/api";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PrimeReactProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PrimeReactProvider>
   </StrictMode>
 );
